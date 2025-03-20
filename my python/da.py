de9c0afcded20f3213cf0c71d50b5109a7bf5019
Hima100100 <hima1001009@gmail.com>
2025-03-21 02:00:11 +0300

    Add files via upload

---
+++ my python/da.py
@@ -0,0 +1,12 @@
+temp =int(input("enter number :  ")) 
+if temp == 30 :
+    print ("It's a hot day. Stay hydrated!")
+elif 20 <= temp <= 29 :
+    print("It's a warm day. Enjoy the weather!")
+elif 10 <= temp <= 19 :
+    print("It's a cool day. Wear a jacket!")
+elif  temp <= 10 :
+    print("It's a cold day. Stay warm")
+else :
+    print ("tank u ")
+   
